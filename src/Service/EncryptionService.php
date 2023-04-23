e156b819f4234528e9e96a1498e6e371ecaa5099
eerzho <eerzho@gmail.com>
2023-04-23 19:00:36 +0200

    change encryption/decryption library

--- src/Service/EncryptionService.php
+++ src/Service/EncryptionService.php
@@ -2,28 +2,26 @@
 
 namespace App\Service;
 
-use ParagonIE\Halite\KeyFactory;
-use ParagonIE\Halite\Symmetric\Crypto;
-use ParagonIE\Halite\Symmetric\EncryptionKey;
-use ParagonIE\HiddenString\HiddenString;
+use Defuse\Crypto\Crypto;
+use Defuse\Crypto\Key;
 use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 
 class EncryptionService
 {
-    private EncryptionKey $encryptionKey;
+    private Key $key;
 
     public function __construct(private readonly ParameterBagInterface $parameterBag)
     {
-        $this->encryptionKey = KeyFactory::loadEncryptionKey($this->parameterBag->get('app.encryption.key_path'));
+        $this->key = Key::loadFromAsciiSafeString($this->parameterBag->get('app.encryption.key'));
     }
 
     public function encrypt(string $message): string
     {
-        return Crypto::encrypt(new HiddenString($message), $this->encryptionKey);
+        return Crypto::encrypt($message, $this->key);
     }
 
     public function decrypt(string $message): string
     {
-        return Crypto::decrypt($message, $this->encryptionKey)->getString();
+        return Crypto::decrypt($message, $this->key);
     }
 }
