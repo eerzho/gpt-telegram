d437f4ee7f1b6db4317d89450fe480341a7b76a9
eerzho <eerzho@gmail.com>
2023-05-01 01:19:14 +0200

    bug fix

--- src/MessageHandler/SendRequestToGptHandler.php
+++ src/MessageHandler/SendRequestToGptHandler.php
@@ -3,20 +3,21 @@
 namespace App\MessageHandler;
 
 use App\Message\SendRequestToGpt;
+use App\Service\ChatTService;
 use App\Service\TelegramApiService;
 use Symfony\Component\Messenger\Attribute\AsMessageHandler;
 
 #[AsMessageHandler]
 final readonly class SendRequestToGptHandler
 {
-    public function __construct(private TelegramApiService $telegramApiService)
+    public function __construct(private TelegramApiService $telegramApiService, private ChatTService $chatTService)
     {
     }
 
     public function __invoke(SendRequestToGpt $message): void
     {
         $this->telegramApiService->sendMessageToCpt(
-            $message->getChatT(),
+            $this->chatTService->getChatByTelegramId($message->getTUserMessage()->getChat()->getId()),
             $message->getTUserMessage(),
             $message->getTWaitMessage()
         );
